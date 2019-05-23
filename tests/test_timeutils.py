fcdf911d38d521c6614e305e8dd418c82418b03a
8118557@qq.com <8118557@qq.com>
2019-05-23 14:40:37 +0800

    updated test_timeutils.py in easyutils/tests

--- tests/test_timeutils.py
+++ tests/test_timeutils.py
@@ -4,10 +4,10 @@ from easyutils.timeutils import is_holiday
 
 def test_is_holiday():
     cases = [
-        ("20161001", True),
-        ("20161008", False),
-        ("20161118", False),
-        ("20161119", True),
+        ("20190501", False),
+        ("20190524", False),
+        ("20190525", True),
+        ("20190526", True),
     ]
     for day, result in cases:
         assert is_holiday(day) == result

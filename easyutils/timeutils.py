f6ce80653ce4919efee5daba0a8723367e03a676
8118557@qq.com <8118557@qq.com>
2019-05-30 20:31:19 +0800

    updated easyquant, easyutils

--- easyutils/timeutils.py
+++ easyutils/timeutils.py
@@ -1,15 +1,28 @@
 # coding:utf8
 import datetime
+import time
+import requests
+
 from datetime import timedelta
 from functools import lru_cache
 
-import requests
 
-import time
+@lru_cache()
+def _is_holiday2(day):
+    """
+    判断是否节假日
+    :param day: 日期， 格式为 '20160404'
+    :return: bool
+    """
+    api = 'http://www.easybots.cn/api/holiday.php'
+    params = {'d': day}
+    rep = requests.get(api, params)
+    res = rep.json()[day if isinstance(day, str) else day[0]]
+    return True if res == "1" else False
 
 
 @lru_cache()
-def is_holiday(day):
+def _is_holiday(day):
     """
     判断是否节假日, api 来自百度 apistore: http://apistore.baidu.com/apiworks/servicedetail/1116.html
     :param day: 日期， 格式为 '20160404'
@@ -22,25 +35,135 @@ def is_holiday(day):
     return True if res != "0" else False
 
 
+def is_holiday(now_time):
+    """
+    判断是否节假日
+    :param now_time: datetime.datetime.now()
+    :return:
+    """
+    day = now_time.strftime("%Y%m%d")
+    return _is_holiday(day)
+
+
 def is_holiday_today():
     """
     判断今天是否时节假日
     :return: bool
     """
-    today = datetime.date.today().strftime("%Y%m%d")
-    return is_holiday(today)
+    return is_holiday(datetime.date.today())
 
 
-def is_tradetime_now():
+def is_weekend(now_time):
     """
-    判断目前是不是交易时间, 并没有对节假日做处理
-    :return: bool
+    判断当前是不是周末
+    :param now_time: datetime.datetime.now()
+    :return:
     """
-    now_time = time.localtime()
-    now = (now_time.tm_hour, now_time.tm_min, now_time.tm_sec)
-    if (9, 15, 0) <= now <= (11, 30, 0) or (13, 0, 0) <= now <= (15, 0, 0):
-        return True
+    return now_time.weekday() >= 5
+
+
+def is_trade_date(now_time):
+    return not (is_holiday(now_time) or is_weekend(now_time))
+
+
+OPEN_TIME = (
+    (datetime.time(9, 15, 0), datetime.time(11, 30, 0)),
+    (datetime.time(13, 0, 0), datetime.time(15, 0, 0)),
+)
+
+PAUSE_TIME = (
+    (datetime.time(11, 30, 0), datetime.time(12, 59, 30)),
+)
+
+CONTINUE_TIME = (
+    (datetime.time(12, 59, 30), datetime.time(13, 0, 0)),
+)
+
+CLOSE_TIME = (
+    (datetime.time(14, 54, 30), datetime.time(15, 0, 0)),
+)
+
+
+def is_trade_time(now_time):
+    """
+    :param now_time: datetime.time()
+    :return:
+    """
+    now = now_time.time()
+    for begin, end in OPEN_TIME:
+        if begin <= now < end:
+            return True
+    else:
+        return False
+
+
+def is_pause(now_time):
+    """
+    :param now_time:
+    :return:
+    """
+    now = now_time.time()
+    for b, e in PAUSE_TIME:
+        if b <= now < e:
+            return True
+
+
+def is_continue(now_time):
+    now = now_time.time()
+    for b, e in CONTINUE_TIME:
+        if b <= now < e:
+            return True
     return False
+
+
+def is_closing(now_time):
+    now = now_time.time()
+    for b, e in CLOSE_TIME:
+        if b <= now < e:
+            return True
+    return False
+
+
+def is_trade_date_now():
+    """
+    判断现在是否是交易日
+    :return:
+    """
+    now_time = datetime.datetime.now()
+    return is_trade_date(now_time)
+
+
+def is_trade_time_now():
+    """
+    判断现在是否是交易日
+    :return:
+    """
+    now_time = datetime.datetime.now()
+    #now_time = time.localtime()
+    return is_trade_time(now_time)
+
+
+def get_next_trade_date(now_time):
+    """
+    :param now_time: datetime.datetime
+    :return:
+    >>> import datetime
+    >>> get_next_trade_date(datetime.date(2016, 5, 5))
+    datetime.date(2016, 5, 6)
+    """
+    now = now_time
+    max_days = 365
+    days = 0
+    while 1:
+        days += 1
+        now += datetime.timedelta(days=1)
+        if is_trade_date(now):
+            if isinstance(now, datetime.date):
+                return now
+            else:
+                return now.date()
+        if days > max_days:
+            raise ValueError('无法确定 %s 下一个交易日' % now_time)
 
 
 def calc_next_trade_time_delta_seconds():
@@ -58,7 +181,7 @@ def calc_next_trade_time_delta_seconds():
         distance_next_work_day = 1
         while True:
             target_day = now_time + timedelta(days=distance_next_work_day)
-            if is_holiday(target_day.strftime("%Y%m%d")):
+            if is_holiday(target_day):
                 distance_next_work_day += 1
             else:
                 break
--- tests/test_timeutils.py
+++ tests/test_timeutils.py
@@ -1,13 +1,57 @@
 import pytest
-from easyutils.timeutils import is_holiday
+from easyutils.timeutils import *
 
 
 def test_is_holiday():
     cases = [
-        ("20190501", False),
-        ("20190524", False),
-        ("20190525", True),
-        ("20190526", True),
+        (datetime.date(2019,5,28), False),
+        (datetime.date(2019,5,29), False),
+        (datetime.date(2019,5,30), False),
+        (datetime.date(2019,5,31), False),
+        (datetime.date(2019,6,1), True),
     ]
     for day, result in cases:
         assert is_holiday(day) == result
+
+
+def test_is_holiday_today():
+    assert is_holiday_today() == False
+
+
+def test_is_weekend():
+    now_time = datetime.datetime.now()
+    assert is_weekend(now_time) == False
+
+
+def test_is_trade_date():
+    now_time = datetime.datetime.now()
+    assert is_holiday(now_time) == False
+    assert is_weekend(now_time) == False
+    assert is_trade_date(now_time) == True
+
+
+def test_is_trade_time():
+    #now_time = datetime.datetime.now()
+    now_time = datetime.datetime(2019,5,30,9,47,24)
+    assert is_trade_time(now_time) == True
+
+    now_time = datetime.datetime(2019, 5, 30, 19, 47, 24)
+    assert is_trade_time(now_time) == False
+
+
+def test_is_trade_now():
+    assert is_trade_date_now() == True
+    assert is_trade_time_now() == False
+
+
+def test_get_next_trade_date():
+    now_time = datetime.datetime.now()
+    next_trade_date = get_next_trade_date(now_time)
+    assert next_trade_date.date() == datetime.date(2019,5,31)
+
+
+def test_calc_next_trade_time_delta_seconds():
+    seconds = calc_next_trade_time_delta_seconds()
+    hours = int(seconds / 3600)
+    minutes = int((seconds - hours * 3600) / 60)
+    assert hours == 13 and minutes == 15
